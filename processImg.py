b098b29d9581e4a3acc434a50d4d12d5b9395bc4
Axfff <61077168+Axfff@users.noreply.github.com>
2023-11-05 14:58:47 +0800

    Update processImg.py

--- processImg.py
+++ processImg.py
@@ -1,26 +1,3 @@
-'''import os
-from PIL import Image
-
-# 输入文件夹路径和输出文件夹路径
-input_folder = "materials/Archive/JGA2023/showphotos"
-output_folder = "materials/Archive/JGA2023/showphotos/converted"
-
-# 如果输出文件夹不存在，则创建它
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-# 遍历输入文件夹中的所有文件
-for filename in os.listdir(input_folder):
-    # 判断文件是否为图片格式（这里只考虑jpg和png格式）
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        # 打开图片文件
-        with Image.open(os.path.join(input_folder, filename)) as im:
-            # 将图片转换为RGB模式
-            im = im.convert("RGB")
-            # 将图片保存为压缩后的jpg格式
-            im.save(os.path.join(output_folder, filename), "JPEG", quality=5, optimize=True)
-'''
-
 import os
 from PIL import Image
 
